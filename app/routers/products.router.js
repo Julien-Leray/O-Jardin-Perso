66c67383733ecc3a540a2243b9c5cdffc76d2efb
Julien Leray <julien.leray@oclock.school>
2024-05-15 15:38:01 +0200

    feat/add-express-server

---
+++ app/routers/products.router.js
@@ -0,0 +1,8 @@
+import express from 'express';
+import Controller from "../controllers/products.controller.js";
+
+const router = express.Router();
+
+router.route('/').get(Controller.getAllProducts);
+
+export default router;
