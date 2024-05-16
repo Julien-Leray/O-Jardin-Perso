cdaeaed3c96cf65cea8edde510d9d078c73c60f8
Julien Leray <julien.leray@oclock.school>
2024-05-16 11:14:35 +0200

    feat: add seeding_v1 and add product(sowing_tips) in BDD

--- app/routers/products.router.js
+++ app/routers/products.router.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import Controller from "../controllers/products.controller.js";
+import Controller from '../controllers/products.controller.js';
 
 const router = express.Router();
 
