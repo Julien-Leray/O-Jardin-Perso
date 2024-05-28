7f244d68f9c4a14025af718a60b5c419fcb5529d
Julien Leray <julien.leray@oclock.school>
2024-05-28 14:00:33 +0200

    feat: joi validation

--- app/controllers/registration.controller.js
+++ app/controllers/registration.controller.js
@@ -2,12 +2,27 @@ import bcrypt from "bcrypt";
 import registrationDatamapper from "../datamappers/registration.datamapper.js";
 import connectionDatamapper from "../datamappers/connection.datamapper.js";
 import asyncHandler from "../middlewares/asyncHandler.middleware.js";
+import joi from "joi";
+
+const userSchema = joi.object({
+  email: joi.string().email().required(),
+  password: joi.string().min(6).required(),
+  firstname: joi.string().required(),
+  lastname: joi.string().required(),
+  address: joi.string().optional(),
+  zip_code: joi.string().pattern(/^0[1-9]\d{3}$|^[1-8]\d{4}$|^9[0-5]\d{3}$|^97[0-5]\d{2}$|^98[6-8]\d{2}$|^98000$/).optional(),
+  city: joi.string().optional(),
+
+});
 
 const controller = {
   registration: asyncHandler(async (req, res) => {
-    let newUser = req.body;
-    if (!newUser.email || !newUser.password || !newUser.firstname || !newUser.lastname) {
-      return res.status(400).json({ message: 'Email, password, firstname and lastname are required' });
+
+    const { error, value: newUser } = userSchema.validate(req.body, { abortEarly: false });
+    console.log(newUser);
+
+    if (error) {
+      return res.status(400).json({ message: 'Invalid input', details: error.details });
     }
     const doesUserExist = await connectionDatamapper.findByEmail(newUser.email);
     if (doesUserExist) {
@@ -18,7 +33,17 @@ const controller = {
     const userCreated = await registrationDatamapper.createUser(newUser);
 
     res.status(201).json(userCreated);
-  })
+  }),
+
+  doesEmailExist: asyncHandler(async (req, res) => {
+    const { email } = req.body;
+    const user = await connectionDatamapper.findByEmail(email);
+    if (user) {
+      res.json({ exists: true });
+    } else {
+      res.json({ exists: false });
+    }
+  }),
 };
 
 export default controller;
--- app/routers/registration.router.js
+++ app/routers/registration.router.js
@@ -4,6 +4,7 @@ import Controller from "../controllers/registration.controller.js";
 const router = express.Router();
 
 router.route('/').post(Controller.registration);
+router.route('/email').post(Controller.doesEmailExist);
 
 export default router;
 
