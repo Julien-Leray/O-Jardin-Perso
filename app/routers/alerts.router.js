ca22557edddb65b711eaf10e4b2af71c27d1248e
Julien Leray <julien.leray@oclock.school>
2024-05-31 13:08:47 +0200

    docs: add an express JsDoc Swagger documentation for API Endpoints

--- app/routers/alerts.router.js
+++ app/routers/alerts.router.js
@@ -2,7 +2,36 @@ import express from 'express';
 import Controller from "../controllers/alerts.controller.js";
 import authMiddleware from '../middlewares/authentification.middleware.js';
 
+/**
+ * An Alert object
+ * @typedef {object} Alert
+ * @property {boolean} watering_alert - The user watering alert status
+ * @property {boolean} forecast_alert - The user forecast alert status
+ */
+
 const router = express.Router();
+
+/**
+ * GET api/me/alerts
+ * @summary Get user alerts
+ * @tags Alerts
+ * @param {string} Authorization.header.required - The token to connect
+ * @returns {Array.<Alert>} 200 - The user alerts
+ * @returns {ApiError} 401 - Unauthorized
+ * @returns {ApiError} 500 - Internal server error
+ */
+
+/**
+ * PATCH api/me/alerts
+ * @summary Update user alerts
+ * @tags Alerts
+ * @param {string} Authorization.header.required - The token to connect
+ * @param {Alert} request.body.required - The alerts to update
+ * @returns {Array.<Alert>} 200 - The user alerts updated
+ * @returns {ApiError} 401 - Unauthorized
+ * @returns {ApiError} 400 - Invalid input
+ * @returns {ApiError} 500 - Internal server error
+ */
 
 router.route('/').get(authMiddleware, Controller.getAlerts);
 router.route('/').patch(authMiddleware, Controller.updateAlert);
--- app/routers/virtualGarden.router.js
+++ app/routers/virtualGarden.router.js
@@ -2,7 +2,78 @@ import express from 'express';
 import Controller from "../controllers/virtualGarden.controller.js";
 import authMiddleware from '../middlewares/authentification.middleware.js';
 
+/**
+ * A virtual garden object
+ * @typedef {object} VirtualGarden
+ * @property {number} user_id - The virtual garden user id
+ * @property {number} product_id - The virtual garden product id
+ * @property {number} quantity - The virtual garden product quantity
+ * @property {array} position - The virtual garden product position
+ */
+
+/**
+ * A virtual garden object to send to the API
+ * @typedef {object} VirtualGardenToSend
+ * @property {number} product_id - The virtual garden product id
+ * @property {number} quantity - The virtual garden product quantity
+ * @property {array} position - The virtual garden product position
+ */
+
+/**
+ * A virtual garden response from the API
+ * @typedef {object} VirtualGardenResponse
+ * @property {array.<VirtualGarden>} products - The virtual garden products
+ */
+
 const router = express.Router();
+
+/**
+ * GET api/virtualGarden
+ * @summary Get all virtual garden products
+ * @tags VirtualGarden
+ * @param {string} Authorization.header.required - Bearer token
+ * @returns {VirtualGardenResponse} 200 - An array of virtual garden products
+ * @returns {ApiError} 500 - Internal server error
+ * @returns {ApiError} 404 - Virtual garden not found
+ */
+
+/**
+ * POST api/virtualGarden
+ * @summary Add a product to the virtual garden
+ * @tags VirtualGarden
+ * @param {string} Authorization.header.required - Bearer token
+ * @param {VirtualGardenToSend} request.body.required - The virtual garden product to add
+ * @returns {VirtualGarden} 201 - The virtual garden product added
+ * @returns {ApiError} 400 - Data to add is required
+ * @returns {ApiError} 400 - Unauthorized
+ * @returns {ApiError} 500 - Internal server error
+ */
+
+/**
+ * DELETE api/virtualGarden/:id
+ * @summary Remove a product from the virtual garden
+ * @tags VirtualGarden
+ * @param {string} Authorization.header.required - Bearer token
+ * @param {number} id.path.required - The virtual garden product id
+ * @returns {ApiSuccess} 204 - Product removed
+ * @returns {ApiError} 400 - Product to remove is required
+ * @returns {ApiError} 400 - Unauthorized
+ * @returns {ApiError} 500 - Internal server error
+ */
+
+/**
+ * PATCH api/virtualGarden/:id
+ * @summary Update a product in the virtual garden
+ * @tags VirtualGarden
+ * @param {string} Authorization.header.required - Bearer token
+ * @param {number} id.path.required - The virtual garden product id
+ * @param {VirtualGardenToSend} request.body.required - The virtual garden product to update
+ * @returns {VirtualGarden} 201 - The virtual garden product updated
+ * @returns {ApiError} 400 - Data to update is required
+ * @returns {ApiError} 401 - Unauthorized
+ * @returns {ApiError} 404 - Product not found or no changes made
+ * @returns {ApiError} 500 - Internal server error
+ */
 
 router.route('/').get(authMiddleware, Controller.getVirtualGarden);
 router.route('/').post(authMiddleware, Controller.addProduct);
